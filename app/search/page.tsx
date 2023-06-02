9bea1626521f3f7a97a007ef4adb9f1365889fa3
andrew-fes <13152155-AndyPetersen90@users.noreply.gitlab.com>
2023-06-02 10:45:09 -0600

    Added new components and component folders to each of the main pages.

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,18 +1,20 @@
-import NavBar from "./components/NavBar";
+import NavBar from "../components/NavBar";
 import Header from "./components/Header";
+import SearchSideBar from "./components/SearchSideBar";
 import RestaurantCard from "./components/RestaurantCard";
 
-export default function Home() {
+export default function Search() {
   return (
     <main className="bg-gray-100 min-h-screen w-screen">
       <main className="max-w-screen-2xl m-auto bg-white">
         <NavBar />
-        <main>
-          <Header />
-          <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
+        <Header />
+        <div className="flex py-4 m-auto w-2/3 justify-between items-start">
+          <SearchSideBar />
+          <div className="w-5/6">
             <RestaurantCard />
           </div>
-        </main>
+        </div>
       </main>
     </main>
   );
