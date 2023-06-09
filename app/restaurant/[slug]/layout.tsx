d32a93b850772e6b40305b15428b90231d129970
andrew-fes <13152155-AndyPetersen90@users.noreply.gitlab.com>
2023-06-09 16:30:14 -0600

    wip: connected to db, started dynamic display for restaurants and menus.

--- app/restaurant/[slug]/layout.tsx
+++ app/restaurant/[slug]/layout.tsx
@@ -1,14 +1,14 @@
 import Header from "./components/Header";
 
-export default function RestaurantLayout({ children }: { children: React.ReactNode }) {
+export default function RestaurantLayout({ children, params }: { children: React.ReactNode; params: { slug: string } }) {
   return (
     <main>
-      <Header />
+      <Header name={params.slug} />
       <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">{children}</div>
     </main>
   );
 }
 
-export const metadata = {
-  title: "Milesstones Grill | OpenTable",
-};
+// export const metadata = {
+//   title: "Milesstones Grill | OpenTable",
+// };
--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -6,16 +6,50 @@ import Description from "./components/Description";
 import Images from "./components/Images";
 import Reviews from "./components/Reviews";
 import ReservationCard from "./components/ReservationCard";
+import { PrismaClient } from "@prisma/client";
 
-export default function RestaurantDetails() {
+const prisma = new PrismaClient();
+
+interface RestaurantType {
+  id: number;
+  name: string;
+  images: string[];
+  description: string;
+  slug: string;
+}
+
+const fetchRestaurantBySlug = async (slug: string): Promise<RestaurantType> => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+    select: {
+      name: true,
+      id: true,
+      images: true,
+      description: true,
+      slug: true,
+    },
+  });
+
+  if (!restaurant) {
+    throw new Error();
+  }
+
+  return restaurant;
+};
+
+export default async function RestaurantDetails({ params }: { params: { slug: string } }) {
+  const restaurant = await fetchRestaurantBySlug(params.slug);
+
   return (
     <>
       <div className="bg-white w-[70%] rounded p-3 shadow">
-        <RestaurantNavBar />
-        <Title />
+        <RestaurantNavBar slug={restaurant.slug} />
+        <Title name={restaurant.name} />
         <Rating />
-        <Description />
-        <Images />
+        <Description description={restaurant.description} />
+        <Images images={restaurant.images} />
         <Reviews />
       </div>
       <div className="w-[27%] relative text-reg">
@@ -28,5 +62,4 @@ export default function RestaurantDetails() {
 export const metadata = {
   title: "Menu | Milesstones Grill | OpenTable",
 };
-
-//iLuNcxVT6fpyQYhi
+/*"tailwindcss": "^3.2.4"*/
