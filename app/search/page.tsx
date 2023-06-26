cfc4b1f4f1a4e6e4700e8653097693a500989ce4
andrew-fes <13152155-AndyPetersen90@users.noreply.gitlab.com>
2023-06-26 09:26:59 -0600

    WIP: updates to prisma doc and dynamic functionality added to searchbar, page, restaurantCard, and searchsidebar.

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,15 +1,80 @@
 import Header from "./components/Header";
 import SearchSideBar from "./components/SearchSideBar";
 import RestaurantCard from "./components/RestaurantCard";
+import { PRICE, PrismaClient } from "@prisma/client";
 
-export default function Search() {
+const prisma = new PrismaClient();
+interface SearchParamsType {
+  city?: string;
+  cuisine?: string;
+  price?: PRICE;
+}
+
+const fetchRestaurantsByCity = (searchParams: SearchParamsType) => {
+  const where: any = {};
+
+  if (searchParams.city) {
+    where.location = {
+      name: {
+        equals: searchParams.city.toLowerCase(),
+      },
+    };
+  }
+  if (searchParams.cuisine) {
+    where.cuisine = {
+      name: {
+        equals: searchParams.cuisine.toLowerCase(),
+      },
+    };
+  }
+  if (searchParams.price) {
+    where.price = {
+      equals: searchParams.price,
+    };
+  }
+
+  const select = {
+    id: true,
+    name: true,
+    main_image: true,
+    price: true,
+    cuisine: true,
+    location: true,
+    slug: true,
+  };
+
+  if (!searchParams.city) return prisma.restaurant.findMany({ select });
+
+  return prisma.restaurant.findMany({
+    where,
+    select,
+  });
+};
+
+const fetchLocations = async () => {
+  return prisma.location.findMany();
+};
+
+const fetchCuisines = async () => {
+  return prisma.cuisine.findMany();
+};
+
+export default async function Search({ searchParams }: { searchParams: SearchParamsType }) {
+  const restaurants = await fetchRestaurantsByCity(searchParams);
+  const locations = await fetchLocations();
+  const cuisines = await fetchCuisines();
+
   return (
     <>
       <Header />
       <div className="flex py-4 m-auto w-2/3 justify-between items-start">
-        <SearchSideBar />
+        <SearchSideBar locations={locations} cuisines={cuisines} searchParams={searchParams} />
         <div className="w-5/6">
-          <RestaurantCard />
+          {restaurants.length ? (
+            restaurants.map((restaurant) => <RestaurantCard key={restaurant.id} restaurant={restaurant} />)
+          ) : (
+            <p>Sorry, we found no restaurants in this area.</p>
+          )}
         </div>
       </div>
     </>
